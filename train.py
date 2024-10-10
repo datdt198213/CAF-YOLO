5c8c1a3e5a90086894ebd0be6ed77bffc418307e
Đặng Đạt <73807126+datdt198213@users.noreply.github.com>
2024-10-10 09:02:44 +0700

    Update train.py

--- train.py
+++ train.py
@@ -19,9 +19,9 @@ import multiprocessing
 def main():
     # model = YOLO("/content/CAF-YOLO/ultralytics/cfg/models/v8/yolov8-CAFMAttention.yaml")  # build a new model from scratch
     # model = YOLO("/content/CAF-YOLO/runs/detect/train2/weights/best.pt")  # load a pretrained model (recommended for training)
-    model = YOLO("/content/best_game.pt")  # load a pretrained model (recommended for training)
+    model = YOLO("best_game.pt")  # load a pretrained model (recommended for training)
         # Train the model
-    results = model.train(data="/content/datasets/game_class.yaml", epochs=30, imgsz=640)
+    results = model.train(data="/datasets/game_class.yaml", epochs=30, imgsz=640)
 
 if __name__=='__main__':
     multiprocessing.freeze_support()
